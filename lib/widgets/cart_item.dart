ca2fb9ab3a613396e6669b2373cb6b13e60c8351
ebrah <ebrahim.cse.bu@gmail.com>
2023-05-02 13:45:00 +0600

    Finished project

---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,82 @@
+import 'package:ecommerce_user_app13/models/cart_model.dart';
+import 'package:ecommerce_user_app13/utils/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  final CartModel cartModel;
+  final num priceWithQuantity;
+  final VoidCallback onIncrease, onDecrease, onDelete;
+  const CartItem({
+    Key? key,
+    required this.cartModel,
+    required this.priceWithQuantity,
+    required this.onIncrease,
+    required this.onDecrease,
+    required this.onDelete,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 5,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(cartModel.imageUrl!),
+              ),
+              title: Text(cartModel.productName!),
+              subtitle: Text('$currencySymbol${cartModel.salePrice}'),
+              trailing: Text(
+                '$currencySymbol$priceWithQuantity',
+                style:
+                const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.remove_circle,
+                      color: Theme.of(context).primaryColor,
+                      size: 35,
+                    ),
+                    onPressed: onDecrease,
+                  ),
+                  Text(
+                    '${cartModel.quantity}',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.add_circle,
+                      color: Theme.of(context).primaryColor,
+                      size: 35,
+                    ),
+                    onPressed: onIncrease,
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Theme.of(context).errorColor,
+                      size: 35,
+                    ),
+                    onPressed: onDelete,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,89 @@
+import 'package:ecommerce_user_app13/models/cart_model.dart';
+import 'package:ecommerce_user_app13/pages/product_details_page.dart';
+import 'package:ecommerce_user_app13/providers/cart_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/product_model.dart';
+import '../utils/constants.dart';
+
+class ProductItem extends StatefulWidget {
+  final ProductModel productModel;
+
+  const ProductItem({Key? key, required this.productModel}) : super(key: key);
+
+  @override
+  State<ProductItem> createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.pushNamed(
+        context,
+        ProductDetailsPage.routeName,
+        arguments: widget.productModel.id,
+      ),
+      child: Card(
+        elevation: 5,
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: Image.network(
+                    widget.productModel.imageUrl!,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.productModel.name!,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+                Text(
+                  '$currencySymbol${widget.productModel.salesPrice}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Consumer<CartProvider>(builder: (context, provider, child) {
+                  final isInCart = provider.isInCart(widget.productModel.id!);
+                  return ElevatedButton.icon(
+                    icon: Icon(
+                      isInCart ? Icons.remove_shopping_cart : Icons.add_shopping_cart,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      if(isInCart) {
+                        provider.removeFromCart(widget.productModel.id!);
+                      } else {
+                        final cartModel = CartModel(
+                          productId: widget.productModel.id,
+                          productName: widget.productModel.name,
+                          salePrice: widget.productModel.salesPrice,
+                          imageUrl: widget.productModel.imageUrl,
+                          stock: widget.productModel.stock,
+                          category: widget.productModel.category,
+                        );
+                        provider.addToCart(cartModel);
+                      }
+                    },
+                    label: Text(isInCart ? 'Remove' : 'Add'),
+                  );
+                })
+              ],
+            ),
+            if(widget.productModel.stock == 0) Container(
+              alignment: Alignment.center,
+              color: Colors.grey.withOpacity(0.5),
+              child: const Text('Out of Stock', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
